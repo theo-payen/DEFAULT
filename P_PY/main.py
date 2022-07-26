38ce6f022878a3db263beb28d5dd51fe8a0e95fd
Theo PAYEN <tt.payen@gmail.com>
2022-07-26 22:39:39 +0200

    Create main.py

---
+++ P_PY/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+class main():
+	def __init__(self):
+        pass
+
+	def un(self):
+		pass
+
+	def deux(self):
+		pass
+
+	def trois(self):
+		pass
+
+
+if __name__ == '__main__':
+	print ("veillez importer le script")
+else:
+	print ("Le script log a été importer avec succès")
